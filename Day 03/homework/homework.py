420dcd8c7cb51d0f4cfa3e47cd0a165d28ccf6e6
demetrepeikrishvili5-lgtm <demetrefeikrishvili5@gmail.com>
2026-02-28 17:26:39 +0400

    second commit

---
+++ Day 03/homework/homework.py
@@ -0,0 +1,30 @@
+# მაგალითად რამეს რომ ამაში წერ "#" და ფითონი ამას არ კითხულობს.
+
+# აქ არ შეიძლება დაუშვა შეცდომები მაგალითად nammme=(deme ეს იქნება შეცდომა და ერორი იქნება.
+
+# აქ არ შიძ₾ება სფეისი იყოს მხოლოდ ესენი _.
+
+# bug არის შეცდომა და debugging არ შესწორება შეცდომის.
+
+#name = "Lika"
+#print(namme) აქ namme არ უნდა იყოს აქ უნდა იყოს name
+
+# number = 5
+#text = " apples" და აქ ბრჭყალები ახლოს უნდა მოვიტანოთ "apples"და ორი ფ არ უნდა
+#result = number + text
+#print("result") აქ ბრჭყალები უნდა ჩაგვემატებინა
+
+#name = liKa 
+#name2 = ("name") + "4" აქ არ უნდა იყოს naame აქ უნდა იყოს name ბრჩხილებით და ბრჭყალებით
+
+#first_user = "Lika" 
+#2nduser = "Giorgi"აქ 2 წინ უნდა იყოს nduser2
+#print("first_user") აქ ბრჭყალები და ქვედა ტირე
+
+#first_name = "Data" აქ ქვედა ტირე გვჭირდებოდა
+#last % name = "random"
+
+
+# first_user = "Lika"
+#2nduser = "Giorgi"აქ 2 წინ უნდა იყოს nduser2
+#print(first_user) აქ ქვედა ტირე უნდა
