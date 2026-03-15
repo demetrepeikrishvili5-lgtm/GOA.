6c2133e22ca7cead61ba4b55326bf66004032c7d
demetrepeikrishvili5-lgtm <demetrefeikrishvili5@gmail.com>
2026-03-15 16:10:27 +0400

    second commit

---
+++ Day 04/classwork/sagadavoodddddddddd/classwork.py
@@ -0,0 +1,16 @@
+# 1)  user_name=input(demetre peikrishvili)
+#user_name= ("demetre feikrishvili")
+#print(user_name)
+#2)   num1=input(1203)
+#num2=input(2142)
+#print(num1+num2)
+#3)   age=input(10)
+
+
+
+#4) age= 10
+#name=("demetre")
+#price=19.5
+#print(type(demetre))
+#print(type(11))
+#print(type(19.5))
