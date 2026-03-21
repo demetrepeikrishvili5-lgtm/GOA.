aa716ca439f830af5acb66d6d173702d399d7858
demetrepeikrishvili5-lgtm <demetrefeikrishvili5@gmail.com>
2026-03-21 16:56:01 +0400

    second commit

---
+++ Day 06/homework/index .py
@@ -0,0 +1,75 @@
+#2) კომენტარების სახით ახსენით რა არის input-ი და output-ი, მოიყავნეთ შესაბამისი მაგალითები.
+
+#outpiti- მაგალითად კლავიატურა მაუსი და სხვა.
+#ხოლო outputi-მაგალითად მონიტორი ყურსასმენები და სხვა. :)
+#3)შექმენით ცვლადი, რომელშიც შეინხავთ input ინსტრუქციით შემოტანილ მნიშვნელობას, შემდეგ შეამოწმებთ თუ რა ტიპის მონაცემი ინახება ამ ცვლადში და დაპრინტავთ.
+
+#name=input("enter name : ")
+#username=input("enter username : ")
+#print("name" + "username")
+#4) თიოთეული მონაცემთა ტიპისთვის (str,int,float), შექმენით 5 ცვლადი და დაუწერეთ კომენტარი თუ რომელ მონაცემთა ტიპს ინახავს ცვლადი.
+
+#stringi. 
+name=("demetre")
+username=("feikrishvili")
+city=("blablabla")
+age="10"
+brand=("bmw")
+
+#integeri.
+num1=1234567890
+num2=67890
+num3=83131
+num4=9934343
+num5=90909
+
+#float.
+price=19,4
+points=15,5
+score=5,5
+rate=9,9
+pieces=6,5
+
+#5)თიოთეული მონაცემთა ტიპისთვის (str,int,float), შექმენით 5 ცვლადი და დაუწერეთ კომენტარი თუ რომელ მონაცემთა ტიპს ინახავს ცვლადი.
+#stringi.
+
+name=(str("enter name : "))
+username=(str("enter username : "))
+city=(str("enter name : "))
+
+#integeri.
+
+points=(int("enter points : "))
+score=(int("enter score : "))
+age=(int("enter age : "))
+
+#floati
+
+grade=(("enter grade : "))
+win=(("enter win : "))
+price=(("enter price :"))
+
+#6)მომხმარებელს შემოატანინეთ ორი სიტყვა, შეინახეთ ისინი ცვლადებში, მოახდინეთ მათი კონკატინაცია და დაბეჭდეთ.
+
+word=("enter word : ")
+name=("enter name : ")
+print("word" + "name")
+
+#7) მომხმარებელს შემოატანინეთ 5 რიცხვი სხვადასხვა დამოუკიდებელი input-ების სახით, თქვენი დავალებაა დაბეჭდოთ მათი საშუალო არითმეტიკული.
+
+num1=input(5678908768976578765678654678654654676567567)
+num2=input(1234567898765432123456789987654321)
+num3=input(8741737173)
+num4=input(3813913)
+num5=input(8294198)
+print("num1" + "num2" + "num3" + "num4" + "num5")
+
+#8) მომხმარებელს შემოატანინეთ სახელი, გვარი, ასაკი, სიმაღლე, წონა და ამ მონაცემების გამოყენებით დაბეჭდეთ ერთი დიდი წინადადება.
+
+name=("enter your name : ")
+username=("enter your username : ")
+age=("enter your age : ")
+height=("enter your height : ")
+weight=("enter your weight : ")
+print("name"+ "username" + "age" + "height" + "weight") 
+
