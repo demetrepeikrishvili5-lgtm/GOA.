850ee0b7255f3be3b2d6475d1a9765b5b2871e94
demetrepeikrishvili5-lgtm <demetrefeikrishvili5@gmail.com>
2026-03-29 19:27:57 +0400

    second commit

---
+++ Day 08/classwork/index.py
@@ -0,0 +1,12 @@
+#1)კომენტარების სახით ახსენით რა არის logical operators, ჩამოწერეთ თითოეული და განმარტეთ რომელი რა დროს რა შედეგს გვიბრუნებს.
+#აი მაგალითად or და and     or-ან   and-და
+# 2) გააკეთეთ ყველა შედარებით ოპერატორებზე 3 მაგალითი.
+print(9 < 2) #false
+print(2<3) #true
+print(15 < 2) #false
+print(2.0 < 3.0) #true
+print(4.0<5.0) # true
+print(9.7<9.8) # true
+# 3)გააკეთეთ 2 მაგალითი logical operator-ებზე.
+print(False and True)  # false
+print(True and True) #true 
