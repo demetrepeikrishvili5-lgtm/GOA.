80eb431e818151009b68bb396966e251dee157a6
demetrepeikrishvili5-lgtm <demetrefeikrishvili5@gmail.com>
2026-05-02 16:14:55 +0400

    second commit

---
+++ Day 12/classwork/index.py
@@ -0,0 +1,15 @@
+#1)
+for i in range(0,100):
+    print(i)
+#2)
+for i in range(100,200,5):
+    print(i)
+
+#3)
+
+for i in range(1,10):
+    print(i)
+
+#4)
+for i in range(0,100,2):
+    print(i)
