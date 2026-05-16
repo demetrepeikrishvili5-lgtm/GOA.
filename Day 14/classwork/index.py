1f4d87430464f900d290ad673c0021098ef2c557
demetrepeikrishvili5-lgtm <demetrefeikrishvili5@gmail.com>
2026-05-16 16:56:18 +0400

    second commit

---
+++ Day 14/classwork/index.py
@@ -0,0 +1,8 @@
+name = (input('enter your name: '))
+if name =='davit':
+    print('gaumarjos davits')
+weather = (input('enter your weather: '))
+if weather =='sunny':
+    print('ვივარჯიშებ გარეთ')
+if weather == 'მოღრუბულია':
+    print('ვივარჯიშებ გარეთ ოღონ მოგვიანებით')
---
+++ Day 14/homework/index.py
@@ -0,0 +1,133 @@
+#1)
+num=(12)
+if num == 12:
+    print('more than 10')
+else:
+    print('less than 10')
+#2
+num=int(input("enter your number : "))
+if num == 15:
+    print('equal to 15')
+else:
+    print('not equal to 15')
+#3
+string=input("enter yor string : ")
+if string == 'group92':
+    print('You are correct')
+else:
+    print('you are wrong')
+#4
+for i in range(50,100,5):
+    print (i)
+#5
+for i in range(1):
+    print('demetre feikrishvili')
+#6
+num=1
+while 50 > num:
+    num +=1
+    print(num)
+#7
+for i in range(0,100):
+    print(i)
+num=1
+while 100 > num:
+    num +=1
+    print(num)
+#8
+for i in range(0,101):
+    print(i)
+num=1
+while 101 > num:
+    num +=1
+    print(num)
+#9
+for i in range(0,20):
+    print(i)
+
+
+
+num=1
+while 100 > num:
+    num +=1
+    print(num)
+#10
+for i in range(100,200,5):
+    print(i)
+
+num=1
+while 200 > num:
+    num +=5
+    print(num)
+
+#11
+for i in range(10,0):
+    print(i)
+
+num=1
+while 10>0:
+    num -=1
+    print(num)
+#12
+num=input('enter your number : ')
+if num == True:
+    print('this is true')
+if num == False:
+    print('this is false')
+if num == 0:
+    print('this is 0')
+#13
+age1=input('enter your age')
+if age1 == 0-12:
+    print('ბავშვი ხარ ')
+age2=input('enter your age')
+if age2 == 12-19:
+    print('მოზარდი/თინეიჯერი ხარ')
+age3=input('enter your age')
+if age3 == 20-64:
+    print('ზრდასრული ხარ')
+age4=input('enter your age')
+if age4 == 65-120:
+    print('ხანში შეული ხარ')
+age5=input('enter your age')
+if age5 == 120+ :
+    print('გურუ ან ჯადოქარი')
+
+#14
+num=input('enter your num')
+num1=input('enter your num1')
+num2=input('enter your num2')
+print('num2')
+#15
+num=int(input('enter your num :'))
+if num == 1:
+    print('ორშაბათი')
+if num == 2:
+    print('სამშაბათი')
+if num == 3:
+    print('ოთხშაბათი')
+if num == 4:
+    print('ხუთშაბათი')
+if num == 5:
+    print('პარასკევი')
+if num == 6:
+    print('შაბათი')
+if num == 17:
+    print('კვირა')
+else:
+    print('არვიცი ეგ რა დღეა')
+#16
+num=int(input('enter your number :'))
+if num == 50:
+    print('50x5')
+else:
+    print('კვადრატში')
+#17
+password=input('enter your password : ')
+if password == 'goa123':
+    print('correct password')
+else:
+    print('incorrect password')
+#18
+num=int(input('enter your number : '))
+print(num)
