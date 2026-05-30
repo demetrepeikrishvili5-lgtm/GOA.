4a403ba5af86f03099745bf9b6b4fe33f8b198fd
demetrepeikrishvili5-lgtm <demetrefeikrishvili5@gmail.com>
2026-05-30 16:08:50 +0400

    second commit

---
+++ Day 16/classwork/index.py
@@ -0,0 +1,9 @@
+#1) 
+numbers = [1,2,3,4,5]
+print(numbers[3])
+#2)
+cars = ['JIGUL','OPEL','BMW','BUGATI','GAZIKI']
+print(cars[0],cars[2])
+#3)
+numbers = [2,4,6,8,10]
+print(numbers[-1],numbers[-3])
